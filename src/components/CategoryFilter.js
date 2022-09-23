c774f9a5bae0c2421bde954d878929ade24cdf58
“Daysha <+>
2022-09-23 15:38:31 -0700

    Done.

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, setCategory}) {
   return (
     <div className="categories">
-      <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+       <h5>Category filters</h5>
+      {categories.map((category) => {
+        return (
+          <button key={category} onClick={() => setCategory(category)}>
+            {category}
+          </button>
+        );
+      })}
     </div>
   );
 }
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
-function NewTaskForm() {
+function NewTaskForm({addTask, categories}) {
+  const [text, setText] = useState('')
+  const [category, setCategory] = useState('Code')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    addTask({ category, text})
+  }
+  
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text" value={text} onChange={(e) => setText(e.target.value)} />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select name="category" value={category} onChange={(e) => setCategory(e.target.value)} >
+        {categories.map((c) => {
+            return (
+              <option key={c} value={c}>
+                {c}
+              </option>
+            );
+          })}
         </select>
       </label>
       <input type="submit" value="Add task" />
